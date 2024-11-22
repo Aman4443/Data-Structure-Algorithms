35193e93dfff2841c43b39e0af6cca07c21fdc1f
AMAN KUMAR JHA <95953071+Aman4443@users.noreply.github.com>
2024-11-22 20:56:46 +0530

    Basics Sorting Algorithm

--- Sorting.java
+++ Sorting.java
@@ -58,7 +58,106 @@ public class Sorting {
         int arr [] = {3,6,2,1,8,7,4,8,3,1};
         selectionSort(arr);
         printArray(arr);
+    }
+}
 
+//Insertion Sort in Java
+public class Sorting {
+    public static void main(String[] args) {
+        int[] array = {29, 10, 14, 37, 13};
+
+        System.out.println("Original Array:");
+        printArray(array);
+
+        insertionSort(array);
+
+        System.out.println("\nSorted Array:");
+        printArray(array);
+    }
+
+    // Method to sort the array using Insertion Sort
+    public static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int key = array[i];
+            int j = i - 1;
+
+            // Shift elements of the array that are greater than the key
+            // to one position ahead of their current position
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = key;
+        }
+    }
+
+    // Helper method to print the array
+    public static void printArray(int[] array) {
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
+
+
+
+// Counting Sorting in java
+
+public class solution {
+
+    public static void main(String[] args) {
+        int[] array = {5, 3, 8, 6, 3, 2, 7};
+
+        System.out.println("Original Array:");
+        printArray(array);
+
+        countingSort(array);
+
+        System.out.println("\nSorted Array:");
+        printArray(array);
+    }
+
+    // Method to perform Counting Sort
+    public static void countingSort(int[] array) {
+        // Find the maximum value in the array
+        int max = findMax(array);
+
+        // Create a count array of size (max + 1)
+        int[] count = new int[max + 1];
+
+        // Count the occurrences of each element
+        for (int num : array) {
+            count[num]++;
+        }
+
+        // Rebuild the sorted array
+        int index = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                array[index++] = i;
+                count[i]--;
+            }
+        }
+    }
+
+    // Helper method to find the maximum value in the array
+    public static int findMax(int[] array) {
+        int max = array[0];
+        for (int num : array) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max;
+    }
+
+    // Helper method to print the array
+    public static void printArray(int[] array) {
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
     }
 }
 
